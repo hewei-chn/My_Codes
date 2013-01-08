278cc080e99a9d8e45a7dba41550d1f660c97b3a
hewei-chn <hewei.chn@gmail.com>
2013-01-08 15:26:59 +0800

    Add an log archive shell script into repo. U can use This Script to Archive logs monthly in gzip mode and back the orig log files in lastmonth dir.

---
+++ log_archive_monthly.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Created by weihe@corp.netease.com 20130108
+# Use This Script to Archive logs monthly in gzip mode and back the orig log files in lastmonth dir.
+# Version v0.1
+
+
+if [ -z $1 ] || [ ! -d $1 ];then
+	echo use $0 /path/have/logs/ to archive logs in the directory.
+	echo In Default we move file into archive dir with gzip compressed, and orig logs into lastmonth.
+	echo We also filtered the nagios check access log :)
+	exit -1;
+fi
+
+#MONTH=`date +"%Y%m" -d "1 month ago"`
+MONTH=`date +"%Y%m"`
+WORKDIR=$1
+ARCHIVEDIR=archive
+BACKUPDIR=lastmonth
+
+cd $WORKDIR
+
+for dir in log logs;do
+	if [ -d $dir ];then
+		cd $WORKDIR/$dir
+		if [ ! -d ${ARCHIVEDIR} ];then
+			mkdir ${ARCHIVEDIR};
+		fi
+		if [ ! -d ${BACKUPDIR} ];then
+			mkdir ${BACKUPDIR};
+		else
+			rm -f ${BACKUPDIR}/*
+		fi
+		for logfile in `ls *${MONTH}* | awk -F. {'print $1'} | sort | uniq`;do
+			echo Process ${logfile}
+			for file in `ls ${logfile}.log.${MONTH}*`;do
+				cat $file | grep -v " (nagios-plugins" | gzip >>${ARCHIVEDIR}/${logfile}.log.${MONTH}.gz
+				mv $file ${BACKUPDIR}/
+			done
+		done
+	fi
+	cd $WORKDIR
+done
