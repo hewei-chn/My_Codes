549694df4186c12bec5abd1d9c114aa1b5a368f2
hewei-chn <hewei.chn@gmail.com>
2013-01-08 15:46:48 +0800

    Fix an Date and add some describe.

--- log_archive_monthly.sh
+++ log_archive_monthly.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Created by weihe@corp.netease.com 20130108
+# Created by weihe#corp.netease.com 20130108
 # Use This Script to Archive logs monthly in gzip mode and back the orig log files in lastmonth dir.
 # Version v0.1
+# You should Add this script in your crontab or run it every month.
 
 
 if [ -z $1 ] || [ ! -d $1 ];then
-	echo use $0 /path/have/logs/ to archive logs in the directory.
-	echo In Default we move file into archive dir with gzip compressed, and orig logs into lastmonth.
-	echo We also filtered the nagios check access log :)
+	echo "use $0 /path/have/logs/ to archive logs in the directory."
+	echo "In Default we move file into archive dir with gzip compressed, and orig logs into lastmonth."
+	echo "We also filtered the nagios check access log :)"
 	exit -1;
 fi
 
-#MONTH=`date +"%Y%m" -d "1 month ago"`
-MONTH=`date +"%Y%m"`
+MONTH=`date +"%Y%m" -d "1 month ago"`
 WORKDIR=$1
 ARCHIVEDIR=archive
 BACKUPDIR=lastmonth
