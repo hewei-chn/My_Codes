fde29ea8f51d9b13c99768e42f582e4460e4edb1
hewei-chn <hewei.chn@gmail.com>
2013-06-13 11:20:02 +0800

    Add scribe_client config files support. Support multi files each file can specificate sysname, rotate mode etc..

--- scribe_client/bin/scribe_client.py
+++ scribe_client/bin/scribe_client.py
@@ -33,30 +33,44 @@ def SignalHandler(sig, id):
 		print 'received signal HUP'
 	elif sig == signal.SIGTERM:
 		print 'received SIGTERM, shutting down'
-		savefileinfo(DATAFILE)
+		savefileinfoall()
 		exit()
 	elif sig == signal.SIGINT:
 		print 'received SIGINT, shutting down'
-		savefileinfo(DATAFILE)
+		savefileinfoall()
 		exit()
 
-def savefileinfo(datafile):
+def savefileinfoall():
 	global file_info
 	for k,v in file_info.items():
-		print k,
-		v.dump()
-	f = open(datafile, "w")
-	cPickle.dump(file_info,f)
-	f.close()
+		savefileinfo(k)
+	return 0
 
-def loadfileinfo(datafile):
+def savefileinfo(filename):
 	global file_info
-	f = open(datafile, "r")
-	file_info = cPickle.load(f)
+	#for k,v in file_info.items():
+	#	print k,
+	#	v.dump()
+	print filename,file_info[filename].getFname(),
+	file_info[filename].dump()
+	datafile = gen_data_fname(filename)
+	f = open(datafile, "w")
+	cPickle.dump(file_info[filename],f)
 	f.close()
-	for k,v in file_info.items():
-		print k,
-		v.dump()
+	return 0
+
+def loadfileinfo(filename):
+	#global file_info
+	datafile = gen_data_fname(filename)
+	if (os.path.exists(datafile) and os.path.getsize(datafile) > 0):
+		f = open(datafile, "r")
+		file_info = cPickle.load(f)
+		f.close()
+	else:
+		return None
+	print filename,file_info.getFname(),
+	file_info.dump()
+	return file_info
 
 def parse_args():
 	"""Parse Args Functions"""
@@ -64,7 +78,7 @@ def parse_args():
 	from optparse import OptionParser
 	usage = "usage: %prog [options] [logname1] [logname2]"
 
-	parser = OptionParser(usage=usage, version='%prog v0.2', description="A Simple tool to monitor somefile.")
+	parser = OptionParser(usage=usage, version='%prog v0.3', description="A Simple tool to monitor somefile.")
 	parser.add_option("-v", "--verbose", action="count",default=0,
 					dest="verbose", help="Verbose mode")
 	parser.add_option("-H", "--host", metavar="a.b.c.d", type='string',default="127.0.0.1",
@@ -73,17 +87,17 @@ def parse_args():
 					dest="server_port", help="Server's Listen Port.")
 	parser.add_option("-n", "--number", metavar="100", type='int',default=100,
 					dest="log_count", help="send logs to server if new log number is more then this.")
-	parser.add_option("-t", "--type", metavar="systemname", type='string',default="test",
-					dest="type", help="Log's System name")
-	parser.add_option("-s", "--subtype", metavar="subsystemname", type='string',
-					dest="type", help="Log's Subsystem name")
+	parser.add_option("-s", "--sysname", metavar="systemname", type='string',default="test",
+					dest="sysname", help="Log's System name")
+	parser.add_option("-S", "--subsysname", metavar="subsystemname", type='string',
+					dest="subsysname", help="Log's Subsystem name")
 	parser.add_option("-f", "--file", metavar="monitored_log_files", type='string',
 					dest="file", help="Log's Subsystem name")
 	parser.add_option("-r", "--rotate", metavar="rotatemode", type='int',
 					dest="rotate", help="RotateMode 1 for move old file to file with data end\n\t 2 for Create New File for new Day.")
-	parser.add_option("-c", "--continue", action = "store_true", default=True,
-					dest="continue_flag", help="send log after we started")
-	parser.add_option("-F", "--full", action = "store_false", 
+	parser.add_option("-c", "--config", metavar="scribe_client.conf", type='string',
+					dest="config", help="spec the config file")
+	parser.add_option("-F", "--full", action = "store_false", default=True,
 					dest="continue_flag", help="send the full log ")
 	
 	(options, args) = parser.parse_args()
@@ -100,10 +114,6 @@ def parse_args():
 			options.server_port = options.server_host.split(":")[1]
 	if options.server_port:
 		if options.verbose: print("Server_port:%d" % (options.server_port))
-	if not options.file or not os.path.isfile(options.file):
-		print("You must specific an regular input file")
-		parser.print_help()
-		exit(3)
 
 	options.file = os.path.realpath(options.file)
 	fname = options.file
@@ -122,6 +132,7 @@ def parse_args():
 	elif options.rotate < 0:
 		print "Rotate Mode Can't be Less then 1"
 		exit(4) 
+	return options
 
 def filename_detech_rotate(fname):
 	expname=fname.split('.')[-1]
@@ -139,12 +150,12 @@ class SendLog():
 		fullfname=fname
 		fname=os.path.basename(fullfname)
 		if not file_info.has_key(fname):
-			print("File %s add to monitor logs.", fname)
-			file_info[fname] = FileInfo()
+			print("File %s not in monitor logs. Check you configs.", fname)
+			#file_info[fname] = FileInfo(fname)
 			return;
 		if fsize < file_info[fname]:
 			print("File %s is been tracated?", fname)
-			file_info[fname] = FileInfo()
+			file_info[fname].setOffset(0)
 			return;
 		fobj = open(fname)
 		fobj.seek(file_info[fname].getOffset())
@@ -159,84 +170,120 @@ def gen_new_fname(fname, rotate, rotate_days = 1):
 	if rotate == 1:
 		return fname
 	if rotate == 2:
-		expname=fname.split('.')[-1]
+		expname=os.path.basename(fname).split('.')[-1]
 		nextday=time.strftime('%Y%m%d',time.localtime(time.time()+3600*24*rotate_days))
 		fname = fname.replace(expname, nextday)
 		return fname
 	return
 
+def gen_data_fname(fname):
+	datafname = ""
+	names = fname.split(".")
+	for subname in names:
+		if (not subname.isdigit()):
+			datafname = datafname + subname + "."
+		else:
+			break
+	datafname = datafname.replace('/', '_')[1:] + "data"
+	return os.path.join(DATAFILEPATH, datafname)
+
 class FileInfo:
-	global options
-	def __init__(self, offset = 0, log_count = 0):
-		self.offset = offset
-		self.log_count = log_count
-		self.time = time.time()
+	global g_options
+	def __init__(self, fname, sysname = None, offset = 0, log_count = 0, rotate = 0):
+		self.__offset = offset
+		self.__log_count = log_count
+		self.__time = time.time()
+		self.__fname = fname
+		if rotate == None:
+			self.__rotate = g_options.rotate
+		else:
+			self.__rotate = rotate
+		if sysname == None:
+			self.__sysname = g_options.sysname
+		else:
+			self.__sysname = sysname
 	def isTimeout(self):
 		#print "time=%0.2f self.time=%02.f"%(time.time(), self.time)
-		return (time.time() - self.time) > 5
+		return (time.time() - self.__time) > 5
 	def isNeedLog(self):
-		self.log_count += 1
-		return self.log_count > options.log_count;
+		self.__log_count += 1
+		return self.__log_count > g_options.log_count;
 	def setOffset(self, offset, log_count = 0):
-		self.offset = offset;
-		self.time = time.time();
-		self.log_count = log_count;
+		self.__offset = offset;
+		self.__time = time.time();
+		self.__log_count = log_count;
 		return
 	def getOffset(self):
-		return self.offset
+		return self.__offset
 	def getLogcount(self):
-		return self.log_count
+		return self.__log_count
+	def getRotate(self):
+		return self.__rotate
+	def getFname(self):
+		return self.__fname
+	def getCategory(self):
+		return self.__sysname
+	def newFile(self, rotate = -1):
+		if (rotate != -1):
+			if (self.__rotate != 0 and self.__rotate != rotate):
+				print "Error Rotate %d, wanted is %d"%(rotate, self.__rotate)
+				return None
+		else:
+			ratote = self.__rotate
+		return FileInfo(fname = gen_new_fname(self.__fname, self.__rotate), rotate = rotate)
+		
 	def dump(self):
-		print "offset = %u, log_count = %d, time = %d"%(self.offset, self.log_count, self.time)
+		print self.__offset, self.__log_count, self.__time, self.__rotate, self.__fname
+		print "offset = %u, log_count = %d, time = %d, rotate = %d, fullname = %s"%(self.__offset, self.__log_count, self.__time, self.__rotate, self.__fname)
 
 
 class MyEventHandler(pyinotify.ProcessEvent):
 	global file_info
-	global options
+	global g_options
 	def __init__(self, fname, rotate, log_class):
 		pyinotify.ProcessEvent.__init__(self)
 		self.rotate = rotate
-		self.fname = os.path.basename(fname)
-		self.path = os.path.dirname(os.path.realpath(fname))
-		self.newfname = []
+		self.fname = fname
+		#self.path = os.path.dirname(os.path.realpath(fname))
+		self.newfname = {}
 		self.log_class = log_class
 		self.init_new_file()
 	def init_new_file(self):
-		if not self.rotate or self.rotate == 0:
-			for i in range(1,ROTATE_MAX):
-				newname=gen_new_fname(self.fname,i)
+		for fname,v in file_info.items():
+			if file_info[fname].getRotate() == 0:
+				for i in range(1,ROTATE_MAX):
+					newname=gen_new_fname(fname,i)
+					if not newname in self.newfname.keys():
+						self.newfname[newname] = file_info[fname].newFile(rotate=i)
+			else:
+				newname=gen_new_fname(fname,file_info[fname].getRotate())
 				if not newname in self.newfname:
-					self.newfname.append(newname)
-		else:
-			newname=gen_new_fname(self.fname,self.rotate)
-			if not newname in self.newfname:
-				self.newfname.append(newname)
+					self.newfname[newname] = file_info[fname].newFile()
 		return
 	def check_file_timout(self):
 		for fname,node in file_info.items():
 			#print fname,node
 			if file_info[fname].isTimeout():
-				print ("%s/%s is TimeOut"%(self.path,fname))
-				self.log_class.file_send_log(os.path.join(self.path, fname))
+				print ("%s is TimeOut"%(fname))
+				self.log_class.file_send_log(fname)
 	def process_IN_MODIFY(self, event):
 		self.check_file_timout()
-		if (options.verbose):
+		if (g_options.verbose):
 			print event
 		if event.dir:
 			return
-		fname = os.path.basename(event.pathname)
-		fullfname=event.pathname
+		fname = event.pathname
 		if file_info.has_key(fname):
 			#print self.log_class
 			if not file_info[fname].isNeedLog():
 				return
 			else:
-				self.log_class.file_send_log(fullfname)
+				self.log_class.file_send_log(fname)
 		else:
 			print "Ignore File Change of %s"%fname
 	def process_IN_MOVED_TO(self, event):
 		self.check_file_timout()
-		if (options.verbose):
+		if (g_options.verbose):
 			print event
 		if not event.src_pathname:
 			return
@@ -244,33 +291,33 @@ class MyEventHandler(pyinotify.ProcessEvent):
 			return
 		if os.path.dirname(event.pathname) != os.path.dirname(event.src_pathname):
 			return
-		orig_fname = os.path.basename(event.src_pathname)
-		fname = os.path.basename(event.pathname)
-		fullfname = event.pathname
+		orig_fname = event.src_pathname
+		fname = event.pathname
 		if file_info.has_key(orig_fname):
 			file_info[fname] = file_info[orig_fname]
 			del file_info[orig_fname]
-			self.log_class.file_send_log(fullfname)
+			self.log_class.file_send_log(fname)
 			del file_info[fname]
-			if (options.verbose):
+			if (g_options.verbose):
 				print "Move File %s to %s"%(orig_fname, fname)
 	def process_IN_CREATE(self, event):
 		self.check_file_timout()
-		if (options.verbose):
+		if (g_options.verbose):
 			print event
 		if event.dir:
 			return
-		fullfname=event.pathname
-		fname=os.path.basename(event.pathname)
-		if fname in self.newfname:
+		fname=event.pathname
+		if fname in self.newfname.keys():
 			print("Get the New File %s", event.pathname)
-			self.newfname.remove(fname)
-			self.init_new_file()
-			file_info['fname'] = FileInfo()
-			self.log_class.file_send_log(fullfname)
+			newfileinfo = self.newfname.pop(fname)
+			newname = gen_new_fname(newfileinfo.getFname(), newfileinfo.getRotate())
+			self.newfname['newname'] = newfileinfo.newFile();
+			#self.init_new_file()
+			file_info[fname] = newfileinfo
+			self.log_class.file_send_log(fname)
 	def process_default(self, event):
 		self.check_file_timout()
-		if (options.verbose):
+		if (g_options.verbose):
 			print event
 		 
 		
@@ -278,7 +325,7 @@ class MyEventHandler(pyinotify.ProcessEvent):
 
 class ScribeClient(SendLog):
 	"""Python Client For Scribe_Client."""
-	global options
+	global g_options
 	global file_info
 	def __init__(self):
 		print "init"
@@ -301,15 +348,15 @@ class ScribeClient(SendLog):
 		return self.connected
 	def file_send_log(self, fname):
 		fullfname = fname;
-		fname=os.path.basename(fullfname)
+		fname=fullfname
 		fsize = os.path.getsize(fullfname)
 		if not file_info.has_key(fname):
-			print("File %s add to monitor logs."%fname)
-			file_info[fname] = FileInfo()
+			print("File %s didn't in monitor files. Check configs."%fname)
+			#file_info[fname] = FileInfo(fname)
 			return;
 		if fsize < file_info[fname]:
 			print("File %s is been tracated?"%fname)
-			file_info[fname] = FileInfo()
+			file_info[fname].setOffset(0)
 			return;
 		fobj = open(fullfname)
 		fobj.seek(file_info[fname].getOffset())
@@ -323,7 +370,7 @@ class ScribeClient(SendLog):
 				return -1
 		buf = fobj.read(BUF_MAX)
 		while buf and len(buf) > 0:
-			log_entry = scribe.LogEntry(category=options.type, message=buf)
+			log_entry = scribe.LogEntry(category=file_info[fname].getCategory(), message=buf)
 			try:
 				self.client.Log(messages=[log_entry])
 			except TTransport.TTransportException, self.thriftEx:
@@ -341,49 +388,157 @@ class ScribeClient(SendLog):
 		file_info[fname].setOffset(fobj.tell())
 		fobj.close()
 
-	def monitorfile(self, file, rotate):
+	def __format_param(self, param):
+		"""
+		@param param: Parameter.
+		@type param: string or int
+		@return: wrap param.
+		@rtype: list of type(param)
+		"""
+		if isinstance(param, list):
+			for p_ in param:
+				yield p_
+		else:
+			yield param
+
+	def monitorfile(self, rotate, file = []):
 		wm = pyinotify.WatchManager()
-		wm.add_watch(os.path.dirname(file), pyinotify.ALL_EVENTS, rec=True)
+		filelist = self.__format_param(file)
+		dir = []
+		for file in filelist:
+			dir.append(os.path.dirname(file))
+		wm.add_watch(dir, pyinotify.ALL_EVENTS, rec=True)
 		eh = MyEventHandler( fname = file, rotate = rotate, log_class = self)
 		notifier = pyinotify.Notifier(wm, eh)
 		notifier.loop()
 		
 	def run(self):
-		self.connect(options.server_host, options.server_port)
-		self.monitorfile(file = options.file, rotate = options.rotate)
+		self.connect(g_options.server_host, g_options.server_port)
+		self.monitorfile(file = g_options.file, rotate = g_options.rotate)
+
+def parse_config(config_file):
+	config = ConfigParser.ConfigParser()
+	config.read(config_file)
+	return config
+
+class Goption():
+	def __init__(self):
+		self.server_host = None
+		self.server_port = None
+		self.file = []
+		self.rotate = None
+		self.verbose = None
+		self.log_count = None
+		self.sysname = None
+		self.continue_flag = None
+	def setup(self, options, config = None):
+		global file_info
+
+		#Init with the default value.
+		self.server_host = options.server_host
+		self.server_port = options.server_port
+		self.verbose = options.verbose
+		self.sysname = options.sysname
+		self.continue_flag = options.continue_flag
+		self.file.append(options.file)
+		self.rotate = options.rotate
+
+		if (options.file and not file_info.has_key(options.file)):
+			fname = options.file
+			local_file_info = None
+			if (self.continue_flag):
+				local_file_info = loadfileinfo(fname)
+				if not local_file_info:
+					print "Load FileInfo For %s Failed, will use current offset"%(fname)
+					local_file_info = FileInfo(fname = fname, rotate = self.rotate, offset = os.path.getsize(fname), sysname = self.sysname)
+			else:
+				print "All the Log of %s will be Send."%(fname)
+				local_file_info = FileInfo(fname = fname, rotate = self.rotate)
+			file_info[fname] = local_file_info
+
+		if (config == None):
+			return
+
+		s = "global"
+		for o in config.options(s):
+			if o == "host":
+				self.server_host = config.get(s, o)
+				if (options.server_host != self.server_host and options.server_host != "127.0.0.1"):
+					print "[host]Options %s Override Config %s"%(options.server_host, self.server_host)
+					self.server_host = options.server_host
+			elif o == "port":
+				self.server_port = config.getint(s, o)
+				if (options.server_port != self.server_port and options.server_port != 1463):
+					print "[port]Options %d Override Config %d"%(options.server_port, self.server_port)
+					self.server_host = options.server_host
+			elif o == "verbose":
+				self.verbose = max(config.getint(s, o), options.verbose)
+			elif o == "system":
+				self.sysname = config.get(s,o)
+				if (options.sysname != self.sysname and options.sysname != "test"):
+					print "[systemname]Options %s Override Config %s"%(options.sysname , self.sysname)
+					self.sysname = options.sysname
+			elif o == "continue":
+				self.continue_flag = config.getboolean(s, o)
+				if (options.continue_flag != self.continue_flag and options.sysname != "test"):
+					print "[systemname]Options %s Override Config %s"%(options.sysname , self.sysname)
+					self.sysname = options.sysname
 		
+		for s in config.sections():
+			if s != "global":
+				fname = s
+				if fname == options.file:
+					print "[File] Options Override Config with file %s"%fname
+					continue
+				self.file.append(fname)
+				rotate = 0
+				continue_flag = True
+				local_file_info = None
+				sysname = self.sysname
+				for o in config.options(s):
+					if o == "rotate":
+						rotate = config.getint(s,o)
+					elif o == "sysname":
+						sysname = config.get(s,o)
+					elif o == "continue":
+						continue_flag = config.getboolean(s, o)
+				if (continue_flag == True or (continue_flag == None and self.continue_flag == True)):
+					local_file_info = loadfileinfo(fname)
+					if not local_file_info:
+						print "Load FileInfo For %s Failed, will use current offset"%(fname)
+						local_file_info = FileInfo(fname = fname, rotate = rotate, offset = os.path.getsize(fname), sysname = sysname)
+				else:
+					print "All the Log of %s will be Send."%(fname)
+					local_file_info = FileInfo(fname = fname, rotate = rotate, sysname = sysname)
+				file_info[fname] = local_file_info
+
+
 
 if __name__ == '__main__':
 	global file_info
-	global options
-	global DATAFILE
+	global g_options
+	global DATAFILEPATH
+	# DATAFILEPATH Must Define Before Parse Options And Config ,Cause some func may use it.
+	datapath=os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "..", "data")
+	if (not os.path.isdir(datapath)):
+		os.mkdir(datapath)
+	DATAFILEPATH=datapath
+
+	g_options = Goption()
 	signal.signal(signal.SIGUSR1, SignalHandler)
 	signal.signal(signal.SIGHUP, SignalHandler)
 	signal.signal(signal.SIGTERM, SignalHandler)
 	signal.signal(signal.SIGINT, SignalHandler)
 	file_info = {}
-	parse_args()
-	fname = options.file
+	options = parse_args()
+	config = None
 
-	datapath=os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "..", "data")
-	if (not os.path.isdir(datapath)):
-		os.mkdir(datapath)
-	if (options.rotate == 2):
-		datafilename=os.path.basename(fname)[0:os.path.basename(fname).rindex('.')]+".data"
-	else:
-		datafilename=os.path.basename(fname)+".data"
-	DATAFILE=os.path.join(datapath, datafilename)
+	if (options.config):
+		config = parse_config(options.config)
 
-	if (options.continue_flag):
-		if (os.path.exists(DATAFILE) and os.path.getsize(DATAFILE) > 0):
-			print "Load FileInfo from fileinfo.data..."
-			loadfileinfo(DATAFILE)
-		if not file_info.has_key(os.path.basename(fname)):
-			print "File Name not in stored Data, Init with current state"
-			file_info[os.path.basename(fname)] = FileInfo(offset=os.path.getsize(fname))
-	else:
-		print "All the Log of %s will be Send."
-		file_info[os.path.basename(fname)] = FileInfo()
+	g_options.setup(options, config)
+
+
 
 	sc = ScribeClient()
 	sc.run()
